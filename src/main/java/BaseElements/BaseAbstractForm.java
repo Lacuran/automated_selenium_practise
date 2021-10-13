833cf5d7a126e630f774075a22bce190b5cac87a
Lacuran <kornel.wrzesien@gmail.com>
2021-10-13 12:57:44 +0200

    13.10.2021 new changes and cleanups

---
+++ src/main/java/BaseElements/BaseAbstractForm.java
@@ -0,0 +1,12 @@
+package BaseElements;
+
+import Core.TAEDriver;
+import Core.TEABaseObject;
+
+public class BaseAbstractForm extends TEABaseObject {
+    TAEDriver driver;
+
+    public BaseAbstractForm(TAEDriver driver) {
+        this.driver = driver;
+    }
+}
